110bbb1292d0e857b47ebcf234988ed8f370f72f
Marius <marius.eggen@geomatikk-ikt.no>
2014-03-26 15:49:11 +0100

    backend for animasjoner #2

--- json_mapper.py
+++ json_mapper.py
@@ -1,4 +1,3 @@
-__author__ = 'marius'
 
 
 import json
@@ -8,6 +7,8 @@ import grid
 import threading
 from os import listdir
 from os.path import isfile, join
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 ledStrip = LedStrip_WS2801(75)
@@ -22,13 +23,13 @@ class RunAnimation(threading.Thread):
         self.stop = 0
 
     def run(self):
+        print("running thread")
         while not self.stop:
             for frames in self.data:
                 for row in range(0, 8):
                     for col in range(0, 9):
-
                         gridnr = grid.grid[row][col]
-                        animnr = frames[row*col]
+                        animnr = frames[(9*row)+col]
                         ledStrip.setPixel(gridnr, animnr)
                 ledStrip.update()
                 sleep(self.msperframe)
@@ -36,8 +37,13 @@ class RunAnimation(threading.Thread):
     def stop(self):
         self.stop = 1
 
+
 def main():
     grid.populateGrid()
+    print "App running on 0.0.0.0:5000"
+    app.debug = True
+    app.run(host='0.0.0.0')
+
 
 
 @app.route("/api/animation")
@@ -45,33 +51,33 @@ def animation_html():
     return render_template("animation.html")
 
 
-
-#json example: {frames:[0:{rgb,rgb,rgb,rgb},1:{rgb,rgb,rgb,rgb}],config{fps:float}}
-@app.route("/api/animation_run")
-def animation_run(jdata):
-#Read json
-
+#json example: {frames:[0:[0:'rgb',1:'rgb',2:'rgb',3:'rgb'},1:{0:'rgb',1:'rgb',2:'rgb',3:'rgb'}],config{fps:float}}
+@app.route("/api/runanimation")
+def runanimation():
+    jdata = request.args.get('jdata', '')
+    global thread
+    #Read json
+    #jdata = request.form["jdata"]
     if thread is not None:
         thread.stop()
 
     json_data = open(jdata)
     data = json.load(json_data)
     json_data.close()
-
     msperframe = 1000/data.config.fps
 
-    thread = RunAnimation(data,msperframe)
+    thread = RunAnimation(data, msperframe)
     thread.start()
 
 @app.route("/api/animation_save")
 def animation_save(name,jdata):
-    myFile = open('/json/'+name+'.json', 'w')
+    myFile = open('json/'+name+'.json', 'w')
     myFile.write(jdata)
     myFile.close()
 
 @app.route("/api/animation_load")
 def animation_load(name):
-    myFile = open('/json/'+name+'.json', 'r')
+    myFile = open('json/'+name+'.json', 'r')
     jdata = myFile.read()
     myFile.close()
     return jdata
@@ -79,7 +85,9 @@ def animation_load(name):
 @app.route("/api/animation_list")
 def animation_run():
 
-    files = [f for f in listdir('/json/') if isfile(join('/json/', f))]
+    files = [f for f in listdir('json/') if isfile(join('/json/', f))]
     return files
 
-main()
+if __name__ == '__main__':
+    print "Now running main"
+    main()
